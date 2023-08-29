1f621fcd794f5f6b57bd801c6e29aaaa48e4e867
aungzawphyo <aungzawphyo1102@gmail.com>
2023-08-29 16:12:23 +0630

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,20 @@ import classes from './Home.module.css'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+  const request = async() => {
+    const response = await fetch('http://13.228.72.15:5000/chat/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: 'aungzawphyo1102@gmail.com', password: 'studyITin2$'})
+    })
+    const resData = await response.json()
+    console.log(resData)
+  }
+
   return (
     <div className={classes.home}>
         <div className={classes.content}>
@@ -11,6 +25,7 @@ const Home = () => {
           <p>For Personal or Bussiness</p>
         </div>
         <div>
+          <button onClick={request}>click</button>
           <Link to='/personal'>Personal</Link>
           <Link to='/bussiness'>Bussiness</Link>
         </div>
